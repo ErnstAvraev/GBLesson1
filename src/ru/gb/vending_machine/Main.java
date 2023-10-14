bf2613798d89bf7f767c276ed80c2920a6f7e01c
Ernst Avraev <avraev.ernst@yandex.ru>
2023-10-14 19:22:11 +0300

    fix

--- src/ru/gb/vending_machine/Main.java
+++ src/ru/gb/vending_machine/Main.java
@@ -10,10 +10,10 @@ public class Main {
 
         Product product1 = new Bottle("coca-cola", 150, 1.5);
         Product product2 = new Product("milko", 100);
-
+        Product product3 = new Product("twix", 70);
         vendingMachine.addProduct(product1);
         vendingMachine.addProduct(product2);
-
+        vendingMachine.addProduct(product3);
         System.out.println(vendingMachine.getProductsInfo());
     }
 }
